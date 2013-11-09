4f1fd9931c86d6eb424ca65342a083919bc81d47
Bree Devries <bd2595a@student.american.edu>
2013-11-09 03:46:45 -0500

    Manual Merge (buttons to actions)

--- gen/com/example/hackyaleandroid/R.java
+++ gen/com/example/hackyaleandroid/R.java
@@ -227,12 +227,17 @@ containing a value of this type.
         public static final int swipe_on=0x7f02002f;
     }
     public static final class id {
-        public static final int action_settings=0x7f050005;
+        public static final int action_settings=0x7f05000a;
         public static final int hybrid=0x7f050004;
+        public static final int linearLayout=0x7f050008;
         public static final int none=0x7f050000;
         public static final int normal=0x7f050001;
+        public static final int plus=0x7f050009;
+        public static final int relativeLayout=0x7f050005;
         public static final int satellite=0x7f050002;
+        public static final int scrollView1=0x7f050007;
         public static final int terrain=0x7f050003;
+        public static final int textView1=0x7f050006;
     }
     public static final class integer {
         public static final int google_play_services_version=0x7f070000;
