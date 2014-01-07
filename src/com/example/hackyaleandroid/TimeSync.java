dc28ef5e836a9b135620b36740ed5f79acb0ea36
Bree Devries <bd2595a@student.american.edu>
2014-01-07 11:01:34 -0500

    Fixed error in TimeSync

--- src/com/example/hackyaleandroid/TimeSync.java
+++ src/com/example/hackyaleandroid/TimeSync.java
@@ -2,9 +2,10 @@ package com.example.hackyaleandroid;
 
 public class TimeSync extends Sync {
 
-	public TimeSync(String nName, boolean changeWifi, boolean changeBlueTooth,
-			int volume, TextMessage text) {
-		super(nName, changeWifi, changeBlueTooth, volume, text);
+	public TimeSync(String nName, boolean changeWifi, boolean wifiOn, boolean changeBlueTooth, boolean blueToothOn,boolean volumeOn, int volume, boolean vibrateOn,  boolean textOn, TextMessage text) {
+		
+		
+		super(nName, changeWifi, wifiOn, changeBlueTooth,blueToothOn, volumeOn, volume, vibrateOn, textOn, text);
 		// TODO Auto-generated constructor stub
 	}
 
